a6591e46e994a2dc2a2c8157370147d1c747cbc5
wataame <87641789+candy-con@users.noreply.github.com>
2022-12-18 16:05:12 +0000

    修正 (12/19/22)

---
+++ basic.js
@@ -0,0 +1,23 @@
+
+var piyo = "iTL"+2022; //javascriptは少々特殊で、文字列と数字をつなげてもうまく対応してくれます。
+console.log(piyo);
+
+var hoge = __Q6__; //6. 変数を定義して、hello World! と出力してみましょう。
+console.log(hoge); 
+
+
+
+var fuga = "iTL"; 
+console.log(fuga);
+fuga = _Q7_; // 7. fugaの中身を"GDSC"に書き換えてみましょう。
+console.log(fuga);
+
+/*
+7. おまけ
+*/
+
+var foobar = 1;
+foobar++; // この書き方はインクリメントといいます。+1する操作です。反対はデクリメント(foobar--;)
+console.log(foobar);
+foobar--;
+console.log(foobar);
